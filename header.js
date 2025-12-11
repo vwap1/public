ba3801fa93f373eff07f24f8ac47cb4ee8a779ea
yestrader <vwap@outlook.kr>
2025-12-11 21:55:49 +0900

    Update header.js

--- header.js
+++ header.js
@@ -1,5 +1,6 @@
+
 (function() {
-   // 현재 파일명+확장자 얻기
+   // 현재 파일명 + 확장자 얻기
    const thisfilefullname = document.URL.substring(document.URL.lastIndexOf('/') + 1, document.URL.length);
    // 현재 파일명 얻기
    const thisfilename = thisfilefullname.substring(thisfilefullname.lastIndexOf('.'), 0);
